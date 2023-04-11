d05258129e91898d678419b61336f22861a51afb
Beatriz Fiochi Soares da Silva <biafiochi@gmail.com>
2023-04-11 18:48:54 +0100

    Organizando arquivos

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -50,38 +50,3 @@ console.log(tdAltura); //td que tem a altura
 console.log(altura); //obter o valor (2.00)
 console.log(imc);*/
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-
-botaoAdicionar.addEventListener("click", function(event){  /*escutar evento*/
-   event.preventDefault(); /*previne comportamentos padrões dos usuários do browser*/
-   var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = imc;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-})

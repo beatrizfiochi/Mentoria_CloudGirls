5dd1f12b37347345b08ae9850bb120ba72b0d879
Beatriz Fiochi <biafiochi@gmail.com>
2023-05-05 09:10:41 +0100

    Projetos aula java

---
+++ Java-codigo/sintaxe_variaveis_e_fluxo/src/sintaxe_variaveis_e_fluxo/TestaConversao.java
@@ -0,0 +1,22 @@
+package sintaxe_variaveis_e_fluxo;
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1270.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+		
+		//int x = 2bilhoes;
+		//long numeroGrande = 321321321897L; 
+		//short numeroPequeno = 2131;
+		//byte b = 127;
+		//float pontoFlutuante = 3.14f;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+}

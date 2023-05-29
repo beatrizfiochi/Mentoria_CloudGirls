4c1d3fc1d261ed42c27fa22031e227a8f0e0a880
Beatriz Fiochi <biafiochi@gmail.com>
2023-05-29 19:55:57 +0100

    Continuação aula java - alura

--- Java-codigo/bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ Java-codigo/bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -6,8 +6,19 @@ public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 	
-		ContaCorrente[]contas = new ContaCorrente[10];
-
+		ContaCorrente[]contas = new ContaCorrente[5];
+		ContaCorrente cc1 = new ContaCorrente(22, 11);
+		contas[0] = cc1;
+		
+		ContaCorrente cc2 = new ContaCorrente(22, 22);
+		contas[1] = cc2;
+		
+		//System.out.println(cc1.getNumero());
+		System.out.println(contas[0].getNumero());
+		
+		ContaCorrente ref = contas[1];
+		
+		System.out.println(ref.getNumero());
 	}
 
 }
